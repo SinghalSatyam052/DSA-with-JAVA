6aa0b2361ce51c772fd10ed16e544b4998f5fe01
Satyam Singhal <satyam.singhal02.07@gmail.com>
2025-03-26 18:01:36 +0530

    Tree Matching CSES

--- CSES-ProblemSet-TreesAlgorithm/TreeMatching.java
+++ CSES-ProblemSet-TreesAlgorithm/TreeMatching.java
@@ -1,54 +1,62 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class TreeMatching {
-    static ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-    static int[][] dp = new int[200001][2];
+    static ArrayList<Integer>[] adj;
+    static int[][] dp;
 
-    public static void solve(int src, int par){
+    public static void solve(int src, int par) {
         dp[src][0] = dp[src][1] = 0;
-
         boolean isLeaf = true;
 
-        for(int child : adj.get(src)){
-            if(par != child){
+        for (int child : adj[src]) {
+            if (child != par) {
                 isLeaf = false;
                 solve(child, src);
             }
         }
 
-        if(isLeaf) return;
+        if (isLeaf) return;
 
-        for(int child : adj.get(src)){
-            if(par != child){
-                dp[src][0]+=Math.max(dp[child][0],dp[child][1]);
-            }
-        }
-
-        for(int child : adj.get(src)){
-            if(child != par){
-                dp[src][1] = Math.max(dp[src][1], 1 + dp[child][0] + dp[src][0] - Math.max(dp[child][0],dp[child][1]));
-            }
-        }
+        int exclude = 0, include = 0;
         
+        for (int child : adj[src]) {
+            if (child != par) {
+                exclude += Math.max(dp[child][0], dp[child][1]);
+            }
+        }
+
+        for (int child : adj[src]) {
+            if (child != par) {
+                include = Math.max(include, 1 + dp[child][0] + exclude - Math.max(dp[child][0], dp[child][1]));
+            }
+        }
+
+        dp[src][0] = exclude;
+        dp[src][1] = include;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        // Fast Input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int n = sc.nextInt();
-        for(int i=0;i<=n;i++) adj.add(new ArrayList<>());
+        adj = new ArrayList[n + 1];
+        dp = new int[n + 1][2];
 
-        
-
-        for(int i=1;i<n;i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            adj.get(a).add(b);
-            adj.get(b).add(a);
+        for (int i = 0; i <= n; i++) {
+            adj[i] = new ArrayList<>();
         }
-        
-       solve(1,0);
-       System.out.println(Math.max(dp[1][0],dp[1][1]));
+
+        for (int i = 1; i < n; i++) {
+            String[] edge = br.readLine().split(" ");
+            int a = Integer.parseInt(edge[0]);
+            int b = Integer.parseInt(edge[1]);
+            adj[a].add(b);
+            adj[b].add(a);
+        }
+
+        solve(1, 0);
+        System.out.println(Math.max(dp[1][0], dp[1][1]));
     }
 }
