543444d04c54261fc68c758fbf098fe31f827e25
Satyam Singhal <82570215+SinghalSatyam052@users.noreply.github.com>
2025-04-15 16:22:02 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (84.59%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,28 +1,25 @@
 class Solution {
-    public int binSear(int[] nums,int target,boolean isFirst){
-        int res = -1;
-        int lo = 0;
-        int hi = nums.length-1;
+    public int[] searchRange(int[] nums, int target) {    
+        int len = nums.length;
+        int lo = 0, hi = len-1;
+
+        int[] ans = {-1,-1};
+
         while(lo<=hi){
             int mid = lo+(hi-lo)/2;
-
             if(nums[mid] == target){
-                res= mid;
-                if(isFirst) hi = mid-1;
-                else lo = mid+1;
+                ans[0] = mid;
+                break;
             }
             else if(nums[mid]>target) hi = mid-1;
             else lo = mid+1;
         }
-        return res;
-    }
-    public int[] searchRange(int[] nums, int target) {    
-        
-        int[] ans = {-1,-1};
 
-        ans[0] = binSear(nums,target,true);
-
-        if(ans[0] != -1) ans[1] = binSear(nums,target,false);
+        if(ans[0] != -1){
+            ans[1] = ans[0];
+            while(ans[0]>0 && nums[ans[0]] == nums[ans[0]-1] ) ans[0] = ans[0]-1;
+            while(ans[1]<len-1 && nums[ans[1]] == nums[ans[1]+1] ) ans[1] = ans[1]+1;
+        }
 
         return ans;
     }
