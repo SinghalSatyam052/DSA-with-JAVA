03f74268bec094181eec0f521d31c940ddb2f837
Satyam Singhal <82570215+SinghalSatyam052@users.noreply.github.com>
2025-04-22 15:26:16 +0530

    Time: 3 ms (95.11%) | Memory: 45.1 MB (45.99%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,4 +1,17 @@
 class Solution {
+
+    public int minHelp(String word1,String word2, int i, int j, Integer[][] dp){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+
+        if(dp[i][j] != null) return dp[i][j];
+
+        if(word1.charAt(i) == word2.charAt(j)) return dp[i][j] = minHelp(word1,word2,i-1,j-1,dp);
+
+        return dp[i][j] = 1 + Math.min(minHelp(word1,word2,i-1,j-1,dp),
+                Math.min(minHelp(word1,word2,i,j-1,dp),minHelp(word1,word2,i-1,j,dp)));
+
+    }
     public int minDistance(String word1, String word2) {
         int n = word1.length(), m = word2.length();
         
@@ -7,21 +20,9 @@ class Solution {
         if(word1.length() == 0 || word2.length() == 0) 
             return Math.max(word1.length(), word2.length());
 
-        int[][] dp = new int[n+1][m+1];
-
+        Integer[][] dp = new Integer[n+1][m+1];
         
-        
-        for(int i=0;i<=n;i++) dp[i][0] = i;
-        for(int j = 0;j<=m;j++) dp[0][j] = j;
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = 1 + Math.min(dp[i-1][j-1],Math.min(dp[i][j-1],dp[i-1][j]));;
-            }
-        }
-
-        return dp[n][m];
+        return minHelp(word1,word2,n-1,m-1,dp);
 
     }
 }
