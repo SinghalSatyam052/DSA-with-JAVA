ba72b056f2be42aba0830d6e5780d2c72d1076b3
Satyam Singhal <82570215+SinghalSatyam052@users.noreply.github.com>
2025-07-27 22:33:14 +0530

    Time: 5 ms (89.00%) | Memory: 44.7 MB (69.90%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -7,15 +7,11 @@ class Solution {
             char ch = s.charAt(i);
             if(hm.containsKey(ch) && hm.get(ch) >= left){
                 left = hm.get(ch) + 1;
-            }else{
-                currLen++;
             }
             hm.put(ch,i);
             currLen = i -left + 1;
             maxLen = Math.max(maxLen,currLen);
         }
-
-        
         return maxLen;
     }
 }
