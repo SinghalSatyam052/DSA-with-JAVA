6c5bd4f927d685929cd84ed98efc4f09b67cd26e
Satyam Singhal <82570215+SinghalSatyam052@users.noreply.github.com>
2025-08-02 18:45:18 +0530

    Time: 2 ms (100.00%) | Memory: 45 MB (62.67%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,21 +2,25 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int l1 = word1.length();
         int l2 = word2.length();
-        int[][] dp = new int[l1+1][l2+1];
+
+        Integer[][] dp = new Integer[l1+1][l2+1];
 
         if(word1.equals(word2)) return 0;
-        if(word1.length() == 0 || word2.length() == 0) return Math.max(l1,l2);
+        if(l1 == 0 || l2 == 0) return Math.max(l1,l2);
 
-        for(int i=0;i<=l1;i++) dp[i][0] = i;
-        for(int j=0;j<=l2;j++) dp[0][j] = j;
+        return help(word1,word2,l1-1,l2-1,dp);
+    }
 
-        for(int i=1;i<=l1;i++){
-            for(int j=1;j<=l2;j++){
-                if(word1.charAt(i-1) != word2.charAt(j-1)) 
-                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
-                else dp[i][j] = dp[i-1][j-1];
-            }
-        }
-        return dp[l1][l2];
+    public int help(String word1,String word2, int i, int j,Integer[][] dp){
+
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        
+        if(dp[i][j] != null) return dp[i][j];
+
+        if(word1.charAt(i) == word2.charAt(j)) return dp[i][j] = help(word1,word2,i-1,j-1,dp);
+
+        return dp[i][j] = 1 + Math.min(help(word1,word2,i-1,j-1,dp),
+        Math.min(help(word1,word2,i,j-1,dp), help(word1,word2,i-1,j,dp)));
     }
 }
