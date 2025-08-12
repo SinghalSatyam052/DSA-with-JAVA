dd32fed2f157f1142edfc1ab2d4468665e692760
Satyam Singhal <82570215+SinghalSatyam052@users.noreply.github.com>
2025-08-12 13:55:51 +0530

    Time: 1 ms (82.25%) | Memory: 42.5 MB (98.82%) - LeetSync

---
+++ 1342-queens-that-can-attack-the-king/queens-that-can-attack-the-king.java
@@ -0,0 +1,100 @@
+class Solution {
+    public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        int[][] board = new int[8][8];
+        for(int[] cord:queens){
+            board[cord[0]][cord[1]] = 1;
+        }
+
+        int i = king[0];
+        int j = king[1];
+
+        //left up
+        while(--i>=0 && --j>=0){
+            if(board[i][j] == 1){
+                ans.add(new ArrayList<>(Arrays.asList(i, j)));
+                break;
+            }
+        }
+        i = king[0];
+        j = king[1];
+
+        //up
+        while(--i>=0){
+            if(board[i][j] == 1){
+                ans.add(new ArrayList<>(Arrays.asList(i, j)));
+                break;
+            }
+        }
+        i = king[0];
+        j = king[1];
+
+        //right up
+        while(--i>=0 && ++j<8){
+            if(board[i][j] == 1){
+                ans.add(new ArrayList<>(Arrays.asList(i, j)));
+                break;
+            }
+        }
+        i = king[0];
+        j = king[1];
+
+        //right
+        while(++j<8){
+            if(board[i][j] == 1){
+                ans.add(new ArrayList<>(Arrays.asList(i, j)));
+                break;
+            }
+        }
+        i = king[0];
+        j = king[1];
+
+        //right bottom
+        while(++i<8 && ++j<8){
+            if(board[i][j] == 1){
+                ans.add(new ArrayList<>(Arrays.asList(i, j)));
+                break;
+            }
+        }
+        i = king[0];
+        j = king[1];
+
+        // bottom
+        while(++i<8){
+            if(board[i][j] == 1){
+                ans.add(new ArrayList<>(Arrays.asList(i, j)));
+                break;
+            }
+        }
+        i = king[0];
+        j = king[1];
+
+        //left bottom
+        while(++i<8 && --j>=0){
+            if(board[i][j] == 1){
+                ans.add(new ArrayList<>(Arrays.asList(i, j)));
+                break;
+            }
+        }
+        i = king[0];
+        j = king[1];
+
+        //left
+        while(--j>=0){
+            if(board[i][j] == 1){
+                ans.add(new ArrayList<>(Arrays.asList(i, j)));
+                break;
+            }
+        }
+        
+        return ans;
+    }
+
+    private boolean find(int i, int j, int[][] queens){
+        for(int[] cord: queens){
+            if(cord[0] == i && cord[1] == j) return true;
+        }
+        return false;
+    }
+}
