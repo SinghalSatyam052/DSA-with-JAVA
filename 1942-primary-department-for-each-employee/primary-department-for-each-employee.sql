d3b2e262d4dcfc920e800f1b4a33c16ebc7e54fb
Satyam Singhal <82570215+SinghalSatyam052@users.noreply.github.com>
2025-08-22 13:15:35 +0530

    Time: 529 ms (68.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT e1.employee_id, e1.department_id
+FROM Employee e1
+JOIN (
+    SELECT employee_id, COUNT(department_id) as cnt
+    FROM Employee
+    GROUP BY employee_id
+) e2
+ON e1.employee_id = e2.employee_id
+WHERE e2.cnt = 1
+OR e1.primary_flag = 'Y';
